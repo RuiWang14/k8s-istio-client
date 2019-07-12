e84c98f20003b63e2454a1f7f0b012a82b2efbab
Rui <sprucewang@163.com>
2019-07-12 14:26:48 +0800

    add customer controller

--- cmd/clientset/main.go
+++ cmd/clientset/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	versionedclient "github.com/RuiWang14/k8s-istio-client/pkg/client/clientset/versioned"
+	informers "github.com/RuiWang14/k8s-istio-client/pkg/client/informers/externalversions"
+
+	"github.com/RuiWang14/k8s-istio-client/pkg/signals"
+	"github.com/golang/glog"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
 	"log"
+	"time"
 )
 
 func main() {
+	flag.Parse()
+
 	kubeconfig := "/Users/Rui/.kube/config"
 	namespace := "default"
 	if len(kubeconfig) == 0 || len(namespace) == 0 {
@@ -30,6 +40,30 @@ func main() {
 	for i := range vsList.Items {
 		vs := vsList.Items[i]
 		log.Printf("Index: %d VirtualService Hosts: %+v\n", i, vs.Spec.GetHosts())
+	}
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	kubeClient, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	exampleClient, err := versionedclient.NewForConfig(restConfig)
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+
+	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	controller := NewController(kubeClient, exampleClient,
+		exampleInformerFactory.Networking().V1alpha3().VirtualServices())
+
+	go exampleInformerFactory.Start(stopCh)
+
+	if err = controller.Run(1, stopCh); err != nil {
+		glog.Fatalf("Error running controller: %s", err.Error())
 	}
 
 }
